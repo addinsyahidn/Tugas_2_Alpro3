4a7aa3674bac3eaa1ad2d28aea4d3fbbf187936d
addinsyahidn <addinsyahidn@gmail.com>
2026-03-19 13:13:45 +0700

    Update soalTugas2.cpp

--- soalTugas2.cpp
+++ soalTugas2.cpp
@@ -1,12 +1,48 @@
-/*
-Mas Dodo memberikan sebuah bilangan empat digit kepada anda untuk mengklasifikasikannya menjadi 4 tipe:
+#include <iostream>
+#include <string>
+#include <cmath>
 
-- Bilangan alfa adalah bilangan yang setiap digitnya membentuk pola monoton naik (membesar setiap digitnya) atau monoton turun (mengecil setiap digitnya).
-- Bilangan beta adalah bilangan yang selisih dua digit pertamanya dengan dua digit terakhirnya minimal 30.
-- Bilangan gamma adalah bilangan yang memenuhi persyaratan bilangan alfa dan beta.
-- Bilangan delta adalah bilangan yang tidak memenuhi ketiga syarat di atas.
+using namespace std;
 
-Buatlah program yang menerima input sebuah bilangan empat digit, lalu mengklasifikasikannya ke dalam salah satu dari empat tipe tersebut.
-*/
+int main() {   
+    string bil;
+    int bilint[2]{0, 0}, count(0);
+    bool alfa, beta;
+    // saya buat variabel bool biar ga pening liatnya
+    do {
+        system("cls");
+        cout << "bilangan 4 digit: ";
+        cin >> bil;
+    } while(bil.length() != 4);
+    // mastikan input 4 digit
 
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+    for (int i(0); i <= 2; i++)
+    // for dari 0 - 2 (3 digit) karena ada 4 digit jadi pas i + 1 ga kelebihan
+        if (bil[i] < bil[i + 1]) count++;
+        // kalo index selatjutnya lebih besar, +1
+        else if (bil[i] > bil[i + 1]) count--;
+        /*
+        kalo lebih kecil -1
+        kalo sama ga dipedulikan, biar count 3 atau -3 hanya ketika berurutan
+        */
+
+    bilint[0] = stoi(bil.substr(0, 2));
+    // ubah ke int dari string index 1, sebanyak 2 char
+    bilint[1] = stoi(bil.substr(2));
+    // ubah ke int dari string index 2, ga dibatasin jumlah charnya
+
+    alfa = count / 3;
+    // kalau count ga sampe 3/-3 bakal (+/-) 0,... karena int desimal bakal dibuang jadi sisa 0 (false)
+    beta = abs(bilint[0] - bilint[1]) > 29;
+    // pake abs biar yang dilihat selisihnya saja dan 2 digit mana yang lebih besar ga dilihat
+
+    if (alfa && beta) cout << "bilangan "<< bil <<" adalah gamma";
+    // gamma dulu karena mencakupi 2 status
+    else if (alfa) cout << "bilangan "<< bil <<" adalah alfa";
+    else if (beta) cout << "bilangan "<< bil <<" adalah beta";
+    // alfa dan beta punya status khusus makanya else if
+    else cout << "bilangan "<< bil <<" adalah delta";
+    // delta nilai yang ga memenuhi lainnya makanya else
+
+    return 0;
+} 
