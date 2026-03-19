66bcd5fe90473486fadd0e4e26419572a76973d8
baktiarpurba542-commits <baktiarpurba542@gmail.com>
2026-03-19 17:44:12 +0700

    Update soalLatihan.cpp

--- soalLatihan.cpp
+++ soalLatihan.cpp
@@ -1,16 +1,44 @@
-/*
-Pak Budi adalah seorang pedagang yang harus mengirim barang setiap hari ke beberapa kota. Dia memiliki tiga pilihan jasa ekspedisi untuk mengirim barang berdasarkan berat dan tujuan pengiriman:
-1. JNE
-- Biaya Rp10.000/kg untuk pengiriman dalam kota.
-- Biaya Rp20.000/kg untuk pengiriman luar kota.
-- Diskon 50% jika berat barang lebih dari 10 kg.
-2. SiCepat
-- Biaya Rp8.000/kg untuk pengiriman dalam kota.
-- Biaya Rp18.000/kg untuk pengiriman luar kota.
-- Gratis ongkir jika berat barang lebih dari 15 kg.
-3. Gojek
-- Hanya bisa mengirim dalam kota dengan biaya Rp12.000/kg.
-- Diskon Rp10.000 jika berat barang lebih dari 5 kg.
-*/
+#include <iostream>
+using namespace std;
 
+int main() {
+    int ekspedisi, tujuan;
+    float berat, biaya, total;
+
+    cout << "1. JNE\n2. SiCepat\n3. Gojek\nPilih Ekspedisi: ";
+    cin >> ekspedisi;
+    cout << "Berat barang (kg): ";
+    cin >> berat;
+    cout << "Tujuan (1: Dalam Kota, 2: Luar Kota): ";
+    cin >> tujuan;
+
+    if (ekspedisi == 1) 
+        biaya = (tujuan == 1) ? 10000 : 20000;
+        total = biaya * berat;
+        if (berat > 10) {
+            total = total * 0.5; // Diskon 50%
+        }
+    } 
+    else if (ekspedisi == 2) { // SiCepat
+        if (berat > 15) {
+            total = 0;  ongkir
+        } else {
+            biaya = (tujuan == 1) ? 8000 : 18000;
+            total = biaya * berat;
+        }
+    } 
+    else if (ekspedisi == 3) { // Gojek
+        if (tujuan == 2) {
+            cout << "Gojek hanya tersedia untuk Dalam Kota!" << endl;
+            return 0;
+        }
+        total = 12000 * berat;
+        if (berat > 5) {
+            total = total - 10000;  
+        }
+    }
+
+    cout << "Total Biaya: Rp" << total << endl;
+    return 0;
+}
 // Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
